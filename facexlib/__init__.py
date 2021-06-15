d50157112ab0f4e79836fefd4abdbfdc5a40ea93
Xintao <wxt1994@126.com>
2021-06-15 16:11:35 +0800

    resolve pip install problem

---
+++ facexlib/__init__.py
@@ -0,0 +1,9 @@
+# https://github.com/xinntao/BasicSR
+# flake8: noqa
+from .alignment import *
+from .detection import *
+from .recognition import *
+from .tracking import *
+from .utils import *
+from .version import __gitsha__, __version__
+from .visualization import *
--- setup.py
+++ setup.py
@@ -97,6 +97,7 @@ if __name__ == '__main__':
         author_email='xintao.wang@outlook.com',
         keywords='computer vision, face, detection, landmark, alignment',
         url='https://github.com/xinntao/facexlib',
+        include_package_data=True,
         packages=find_packages(exclude=('options', 'datasets', 'experiments', 'results', 'tb_logger', 'wandb')),
         classifiers=[
             'Development Status :: 4 - Beta',
