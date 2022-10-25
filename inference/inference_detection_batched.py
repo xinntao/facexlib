e90d52395f1d831b0fc0386bb12c7cc59bcbb8fe
ernestchu <shchu@citi.sinica.edu.tw>
2022-10-25 09:44:03 +0000

    update

--- inference/inference_detection_batched.py
+++ inference/inference_detection_batched.py
@@ -2,6 +2,7 @@ import argparse
 from glob import glob
 from time import time
 from os import path, makedirs
+import json
 import cv2
 import torch
 import numpy as np
@@ -19,14 +20,19 @@ def main(args):
     for img_path in glob(f'{path.join(args.img_dir, "")}/*.png'):
         imgs.append(torch.tensor(cv2.imread(img_path)))
     with torch.no_grad():
-        bboxes_list = det_net.batched_detect_faces(torch.stack(imgs), 0.97)
-        # x0, y0, x1, y1, confidence_score, five points (x, y)
-        bboxes_list = [np.concatenate(b, axis=-1) for b in zip(*bboxes_list)]
-        print(bboxes_list)
-        print(f'Took {time() - tic:.2f}s to detect {len(imgs)} images.')
+        # [x0, y0, x1, y1, confidence_score], [five points (x, y)]
+        bboxes_list, landmarks_list = det_net.batched_detect_faces(torch.stack(imgs), 0.97)
+        # bboxes_and_landmarks_list = [np.concatenate(b, axis=-1) for b in zip(bboxes_list, landmarks_list)]
         makedirs(args.save_dir, exist_ok=True)
-        for i, (img, bboxes) in enumerate(zip(imgs, bboxes_list)):
-            visualize_detection(img, bboxes, path.join(args.save_dir, f'{i}.png'))
+        for i, (img, bboxes, landmarks) in enumerate(zip(imgs, bboxes_list, landmarks_list)):
+            visualize_detection(img, np.concatenate((bboxes, landmarks), axis=-1), path.join(args.save_dir, f'{i}.png'))
+
+            export = [{ 'bbox': bbox[:-1].astype(int).tolist(),
+                        'conf': round(bbox.astype(float)[-1], 2),
+                        'landmark': landmark.astype(int).tolist()
+                      } for bbox, landmark in zip(bboxes, landmarks)]
+            print(json.dumps(export, separators=(',', ':')))
+        print(f'Took {time() - tic:.2f}s to detect {len(imgs)} images.')
 
 
 if __name__ == '__main__':
